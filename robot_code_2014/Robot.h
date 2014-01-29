afcd12562e48f7136bcd978ebc0c4f5eb96a0976
Kaz Smith <mr.hexor@gmail.com>
2014-01-28 20:45:27 -0600

    Added tentative autonomous shooting code using FIRST's example vision processing code to detect whether or not the goal we are facing is hot

--- robot_code_2014/Robot.h
+++ robot_code_2014/Robot.h
@@ -1,8 +1,29 @@
 #ifndef _ROBOT_H_
 #define _ROBOT_H_
 #include <cmath>
+#include "Vision/RGBImage.h"
+#include "Vision/BinaryImage.h"
 
-class Robot:public IterativeRobot
+//Camera constants used for distance calculation
+#define Y_IMAGE_RES 480		//X Image resolution in pixels, should be 120, 240 or 480
+#define VIEW_ANGLE 49		//Axis M1013
+
+//Score limits used for target identification
+#define RECTANGULARITY_LIMIT 40
+#define ASPECT_RATIO_LIMIT 55
+
+//Score limits used for hot target determination
+#define TAPE_WIDTH_LIMIT 50
+#define VERTICAL_SCORE_LIMIT 50
+#define LR_SCORE_LIMIT 50
+
+//Minimum area of particles to be considered
+#define AREA_MINIMUM 150
+
+//Maximum number of particles to process
+#define MAX_PARTICLES 8
+
+class Robot : public IterativeRobot
 {
 public:
 	
@@ -26,10 +47,34 @@ public:
 	void ControlCamera();
 	void SetPIDs();
 	
-	// autonomous routines
-	void SetPower();
-	void AutoStatus();
-
+	// Vision processing methods and structures
+	
+	// Structure to represent the scores for the various tests used for target identification
+	struct Scores
+	{
+		double rectangularity;
+		double aspectRatioVertical;
+		double aspectRatioHorizontal;
+	};
+	
+	struct TargetReport
+	{
+		int verticalIndex;
+		int horizontalIndex;
+		bool Hot;
+		double totalScore;
+		double leftScore;
+		double rightScore;
+		double tapeWidthScore;
+		double verticalScore;
+	};
+	
+	double computeDistance(BinaryImage *image, ParticleAnalysisReport *report);
+	double scoreAspectRatio(BinaryImage *image, ParticleAnalysisReport *report, bool vertical);
+	bool scoreCompare(Scores scores, bool vertical);
+	double scoreRectangularity(ParticleAnalysisReport *report);
+	double ratioToScore(double ratio);
+	bool hotOrNot(TargetReport target);
 	void LineUpWithGoal();
 	
 protected:
@@ -53,7 +98,9 @@ protected:
 	bool camera_locked;
 
 	bool catapult_cocked;
-	bool catapult_decocked;	
+	bool catapult_decocked;
+	
+	TargetReport target;
 };
 
 #endif
